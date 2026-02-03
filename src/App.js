cce597b860f48f593a03474a47cfbee8ce7a3c49
Matthew Cross <matthewcross1017@gmail.com>
2026-02-02 19:33:10 -0500

    new branch start new icons

--- src/App.js
+++ src/App.js
@@ -1,42 +1,37 @@
-// Filename - App.js
-
 import React from "react";
 import Navbar from "./components/Navbar";
-import {
-    BrowserRouter as Router,
-    Routes,
-    HashRouter,
-    Route,
-} from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Mixes from "./components/pages/Mixes";
 import Interviews from "./components/pages/Interviews";
 import PlaylistPlayer from "./components/pages/PlaylistPlayer";
 
 function App() {
-    // <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/957719626&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/sleepwalkerent" title="SleepWalker Entertainment" target="_blank" style="color: #cccccc; text-decoration: none;">SleepWalker Entertainment</a> · <a href="https://soundcloud.com/sleepwalkerent/sets/mixes" title="Mixes" target="_blank" 
-    const mySoundcloudPlaylist = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/957719626&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true";
-    const myYoutubePlaylist = "https://www.youtube.com/embed/videoseries?list=PLV8HfLfzVplYb4L0sWOuWRSMJP0f5CUnV";
-    return (
-        <Router>
-            <div className="App">
-                    <Routes>
-                        <Route exact path="/"
-                        element={
-                            <> {/* Use a React Fragment if you have multiple elements for the root page */}
-                            <PlaylistPlayer
-                                soundcloudPlaylistUrl={mySoundcloudPlaylist}
-                                youtubePlaylistUrl={myYoutubePlaylist}
-                            />
-                            </>
-                        }
-                        />
-                        <Route path="/mixes" element={<Mixes />} />
-                        <Route path="/interviews" element={<Interviews />} />
-                    </Routes>
-                    <Navbar />
-            </div>
-        </Router>
-    );
+  const mySoundcloudPlaylist =
+    "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/957719626&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true";
+
+  const myYoutubePlaylist =
+    "https://www.youtube.com/embed/videoseries?list=PLV8HfLfzVplYb4L0sWOuWRSMJP0f5CUnV";
+
+  return (
+    <Router>
+      <Navbar />
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PlaylistPlayer
+              soundcloudPlaylistUrl={mySoundcloudPlaylist}
+              youtubePlaylistUrl={myYoutubePlaylist}
+            />
+          }
+        />
+
+        <Route path="/mixes" element={<Mixes />} />
+        <Route path="/interviews" element={<Interviews />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
